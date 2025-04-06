183ac5683da7f95967b54b46e0aa7ad593b416f2
raulpy271 <raulrufino0@gmail.com>
2025-04-06 09:02:02 -0300

    Cria operações CRUD para modelo Order

--- src/main/java/com/shopix/api/controllers/OrderController.java
+++ src/main/java/com/shopix/api/controllers/OrderController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.shopix.api.dtos.OrderCreateDTO;
 import com.shopix.api.dtos.OrderResponseDTO;
+import com.shopix.api.dtos.OrderUpdateDTO;
 import com.shopix.api.services.OrderService;
 
 @RestController
@@ -33,5 +37,37 @@ public class OrderController {
 	{
 		return new ResponseEntity<>(orderService.list(), HttpStatus.OK);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<OrderResponseDTO> show(@PathVariable Long id)
+	{
 
+		try {
+			OrderResponseDTO dto =  orderService.show(id);
+			return new ResponseEntity(dto, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@PatchMapping
+	public ResponseEntity<OrderResponseDTO> update(@RequestBody OrderUpdateDTO dto)
+	{
+		try {
+			return new ResponseEntity<>(orderService.update(dto), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> destroy(@PathVariable Long id)
+	{
+		try {
+			orderService.destroy(id);
+			return new ResponseEntity<>("Ordem deletada com sucesso!", HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
 }
--- src/main/java/com/shopix/api/services/OrderService.java
+++ src/main/java/com/shopix/api/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.shopix.api.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.stereotype.Service;
 
 import com.shopix.api.dtos.OrderCreateDTO;
 import com.shopix.api.dtos.OrderResponseDTO;
+import com.shopix.api.dtos.OrderUpdateDTO;
 import com.shopix.api.entities.Order;
+import com.shopix.api.entities.OrderItem;
 import com.shopix.api.mappers.OrderMapper;
 import com.shopix.api.repository.OrderRepository;
 
@@ -26,5 +29,33 @@ public class OrderService {
 	{
 		List<Order> orders = orderRepository.findAll();
 		return orders.stream().map(OrderMapper::toDTO).toList();
+	}
+	
+	public OrderResponseDTO show(Long id)
+	{
+		Order order = orderRepository
+			.findById(id)
+			.orElseThrow(() -> new RuntimeException("Ordem não encontrada"));
+		return OrderMapper.toDTO(order);
+	}
+	
+	public OrderResponseDTO update(OrderUpdateDTO dto)
+	{
+		Order order = orderRepository
+			.findById(dto.id())
+			.orElseThrow(() -> new RuntimeException("Ordem não encontrada"));
+		order.setStatus(dto.status());
+		order.setTotalPrice(dto.totalPrice());
+		order.setPaymentMethod(dto.paymentMethod());
+		order.setTrackingCode(dto.trackingCode());
+		return OrderMapper.toDTO(orderRepository.save(order));
+	}
+	
+	public void destroy(Long id)
+	{
+		Order order = orderRepository
+			.findById(id)
+			.orElseThrow(() -> new RuntimeException("Ordem não encontrada"));
+		orderRepository.delete(order);
 	}
 }
